9211feb4fff94d92f16ff935f4e39e3bbdd7b87e
Baribor Saturday <saturdaybaribor@gmail.com>
2023-07-12 12:28:06 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - The main entry of the shell.
+ * @argc: Number of command line arguments passed
+ * @argv: Values of the argument
+ * Return: 0 if successful.
+ */
+int main(int argc, char const *argv[])
+{
+
+	return (0);
+}
